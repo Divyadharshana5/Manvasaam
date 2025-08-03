c552364add038e4a697ed47b3a594a8e069e7f0b
Dharsha <divyadharshana3@gmail.com>
2025-08-03 07:21:22 +0000

    change the logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,7 @@ export function ManvaasamLogo(props: Omit<React.ComponentProps<typeof Image>, 's
   return (
     <Image
       {...rest}
-      src="https://storage.googleapis.com/aai-web-samples/community-images/manvaasam/logo_2.png"
+      src="https://storage.googleapis.com/aai-web-samples/community-images/manvaasam/logo_3.png"
       alt="Manvaasam Logo"
       width={width}
       height={height}
