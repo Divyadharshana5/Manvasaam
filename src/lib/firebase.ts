f9dbebb3eb231a5b972bb557ecb3f33ccfc8e4ef
Dharsha <divyadharshana3@gmail.com>
2025-08-04 14:20:51 +0000

    same issue is appears

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp, getApps, getApp, FirebaseApp} from 'firebase/app';
-import {getAuth, Auth} from 'firebase/auth';
+"use client";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
+import { getAuth, Auth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCX7QqJ2E-AsLqKiT2YmWYYqQ4PYFR7REo",
@@ -8,17 +8,11 @@ const firebaseConfig = {
   projectId: "agrilink-h50ej",
   storageBucket: "agrilink-h50ej.firebasestorage.app",
   messagingSenderId: "901229086413",
-  appId: "1:901229086413:web:b75f25596f1d7f751d4b84",
+  appId: "1:901229086413:web:b75f25596f1d7b84",
 };
 
 // Initialize Firebase for SSR
-let app: FirebaseApp;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
-}
-
-const auth = getAuth(app);
+const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth: Auth = getAuth(app);
 
 export { app, auth };
