00412323bcd2a51ec560291e2257ac6cc9a8a5db
Dharsha <divyadharshana3@gmail.com>
2025-08-06 06:22:16 +0000

    set bg-agri.png this in our website logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,9 +3,10 @@ export function ManvaasamLogo(props: React.ImgHTMLAttributes<HTMLImageElement>) 
  const { width = 32, height = 32, ...rest } = props;
   return (
     <img
-      src="/Public/bg-agri.png"
+      src="/bg-agri.png"
       width={width}
       height={height}
+      alt="Manvaasam Logo"
       {...rest}
       className="drop-shadow-lg"
     />
