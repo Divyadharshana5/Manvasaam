b3d086afd9c8704c8e16fa7139114388452d30a5
Dharsha <divyadharshana3@gmail.com>
2025-08-06 06:38:36 +0000

    updated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { ManvaasamLogo } from "@/components/icons";
+import Image from "next/image";
 import Link from "next/link";
-
+import Logo from "../../../Public/bg-agri.png"
 export default function AuthLayout({
   children,
 }: {
@@ -9,8 +10,9 @@ export default function AuthLayout({
   return (
     <div className="flex min-h-screen flex-col items-center justify-center auth-layout-background">
       <Link href="/" className="absolute top-8 left-8 flex items-center gap-2">
-        <ManvaasamLogo width={32} height={32} className="text-primary" />
-        <span className="text-xl font-bold text-primary">Manvaasam</span>
+        {/* <ManvaasamLogo width={32} height={32} className="text-primary" /> */}
+         <span className="text-xl font-bold text-primary">Manvaasam</span>  
+        <Image src={Logo} alt="" width={40} height={40} />
       </Link>
       {children}
     </div>
