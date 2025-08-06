4523280d3fe12767f29ad53b3cde991e1b140d07
Dharsha <divyadharshana3@gmail.com>
2025-08-06 06:41:44 +0000

    i want the logo left side and the Manvaasam title in right of the logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
+
 import { ManvaasamLogo } from "@/components/icons";
-import Image from "next/image";
 import Link from "next/link";
-import Logo from "../../../Public/bg-agri.png"
+
 export default function AuthLayout({
   children,
 }: {
@@ -10,9 +10,8 @@ export default function AuthLayout({
   return (
     <div className="flex min-h-screen flex-col items-center justify-center auth-layout-background">
       <Link href="/" className="absolute top-8 left-8 flex items-center gap-2">
-        {/* <ManvaasamLogo width={32} height={32} className="text-primary" /> */}
-         <span className="text-xl font-bold text-primary">Manvaasam</span>  
-        <Image src={Logo} alt="" width={40} height={40} />
+        <ManvaasamLogo width={40} height={40} />
+        <span className="text-xl font-bold text-primary">Manvaasam</span>
       </Link>
       {children}
     </div>
