9c12a08ac1bdea6e661d9ee4993ed601d2b466e3
Dharsha <divyadharshana3@gmail.com>
2025-08-07 04:27:49 +0000

    set ../../../Public/bg-agri.png this in our project logo,set left set of

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+
+import { ManvaasamLogo } from "@/components/icons";
 import Link from "next/link";
-import Logo from "../../../Public/bg-agri.png"
+
 export default function AuthLayout({
   children,
 }: {
@@ -9,9 +10,8 @@ export default function AuthLayout({
   return (
     <div className="flex min-h-screen flex-col items-center justify-center auth-layout-background">
       <Link href="/" className="absolute top-8 left-8 flex items-center gap-2">
-      <Image src={Logo} alt=""  />
-      {/* <span className="text-xl font-bold text-primary">Manvaasam</span>   */}
-       
+        <ManvaasamLogo width={40} height={40} />
+        <span className="text-xl font-bold text-primary">Manvaasam</span>
       </Link>
       {children}
     </div>
