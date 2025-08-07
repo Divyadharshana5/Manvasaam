3ff6d9e42365ba900615b16fe9c0ffb59fe4911c
Dharsha <divyadharshana3@gmail.com>
2025-08-07 05:58:26 +0000

    updated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({
   return (
     <div className="flex min-h-screen flex-col items-center justify-center auth-layout-background">
       <Link href="/" className="absolute top-8 left-8 flex items-center gap-2">
-        <Image src={Logo} width={40} height={40} />
+      <Image src={Logo} width={40} height={40} />
         <span className="text-xl font-bold text-primary">Manvaasam</span>
       </Link>
       {children}
