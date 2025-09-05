2c20f3602d0e0e4608f71b0c11869526fa7f75da
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-09-05 13:23:13 +0530

    Fix typo in nodemailer transport creation function

--- test-email.js
+++ test-email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 async function testEmail() {
-  const transporter = nodemailer.createTransporter({
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
