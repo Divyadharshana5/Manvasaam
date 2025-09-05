ad291b7d1527ec018acb19b2c6978c2a341924eb
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-09-05 12:56:30 +0530

    Update nodemailer configuration to use the correct username for email service

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "YOUR-USERNAME",
+    user: "Manvaasam",
     pass: "THE-GENERATED-APP-PASSWORD",
   },
 });
