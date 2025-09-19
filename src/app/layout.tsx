a72a8e700a692748cf2fc57e80c60e5059329c29
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-09-19 10:02:15 +0530

    refactor: remove GlobalVoiceAssistant component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,8 +158,6 @@ export default function RootLayout({
           <LanguageProvider>
             <NavigationProvider>
               <AuthProvider>{children}</AuthProvider>
-              {/* Voice Assistant always at right bottom, accessible on all pages */}
-              <GlobalVoiceAssistant />
             </NavigationProvider>
           </LanguageProvider>
         </FirebaseErrorBoundary>
